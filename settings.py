cd0f9ee24b07fbd71d2d4c1e8be2a8abe6300bc3
Losishche <agr39@ya.ru>
2016-12-23 15:50:33 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 __author__ = 'grishaev'
 
 postgres_database_settings = {
-        'DB': 'VKSpammer',
-        'USER': 'postgres',
-        'PASSWORD': 'continental',
+        'DB': '*****',
+        'USER': '******',
+        'PASSWORD': '*****',
         'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'PORT': '***'
     }
